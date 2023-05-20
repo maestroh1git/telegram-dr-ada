f38f9f7e077621696d10cf45caebc477c93e49fc
David Amaku <amaku@Davids-MacBook-Pro.local>
2023-05-20 20:46:06 +0100

    Asynchronous, Non-Blocking Update

--- handlers/response_handler.py
+++ handlers/response_handler.py
@@ -2,11 +2,11 @@ from telegram import Update
 from telegram.ext import MessageHandler, filters, CallbackContext
 from services.openai_service import generate_response
 
-def handle_message(update: Update, context: CallbackContext) -> None:
+async def handle_message(update: Update, context: CallbackContext) -> None:
     try:
         message = update.message.text
         response = generate_response(message)
-        update.message.reply_text(response)
+        await update.message.reply_text(response)
     except Exception as e:
         print("An error occurred:", str(e))
 
--- handlers/start_handler.py
+++ handlers/start_handler.py
@@ -5,10 +5,10 @@ from telegram.ext import CommandHandler, CallbackContext
 #     update.message.reply_text('Hello! I am your medical bot. How can I assist you today?')
 
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     first_name = update.effective_user.first_name
     joke = "Why don't scientists trust atoms? Because they make up everything!"
-    update.message.reply_text(
+    await update.message.reply_text(
         f"Hey there, {first_name}! I'm DocBot, your personal medical assistant. "
         f"I'm here to help you with your medical questions. But first, how about a joke? \n\n{joke}\n\n"
         f"Alright, now how can I assist you today?")
